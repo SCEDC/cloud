858f785d7c59fb33e27dc45a2a10d5ac219929cd
Shang-Lin Chen <schen@gps.caltech.edu>
2019-09-04 12:32:14 -0700

    Clean up temporary files and overall code cleanup
    Added code to remove temporary input and output files after processing.
    Added exception handling.
    Added more comments.

--- process.py
+++ process.py
@@ -1,22 +1,41 @@
+"""
+Lambda function that decimates a miniSEED seismogram from
+one S3 bucket and uploads the result to another S3 bucket.
+"""
+
 import os
 import boto3
 import obspy
 from obspy.core.stream import Stream
 
-def parse_key(key):
-
-    filename = os.path.basename(key)
 
 def decimate(infile, outfile, dec_factor):
+    """
+    Runs ObsPy's decimate function on the traces
+    in an input seismogram.
+
+    :param infile: Name of the input file
+    :param outfile: Name of the output file
+    :param dec_factor: Decimation factor
+    """
     st = obspy.read(infile)
     st_new = Stream()
+    # Decimate each trace in the stream.
     for tr in st:
         tr.decimate(factor=dec_factor, strict_length=False)
         st_new.append(tr)
     print(outfile)
+    # Write the resulting stream to disk.
     st_new.write(outfile, format='MSEED')
 
+
 def process(event):
+    """
+    Process input parameters and calls decimation function.
+
+    :param event: Input parameters to Lambda
+    """
+
     key = event['s3_key']
     print('Processing {}'.format(key))
     bkt_out_name = event['s3_output_bucket']
@@ -24,11 +43,6 @@ def process(event):
     dec_factor = event['decimation_factor']
     print('input bucket:{} output bucket:{} decimation:{}'.format(bkt_in_name, bkt_out_name, dec_factor))
     (year, year_day, filename) = key.split('/')
-    (fn, ext) = os.path.splitext(filename)
-
-    if ext != '.ms' and ext != '.mseed':
-        print('{} is not a miniSEED file'.format(key))
-        return
     
     session = boto3.Session()
     s3 = boto3.client('s3', region_name='us-west-2')
@@ -38,13 +52,25 @@ def process(event):
     # Download file from S3.
     s3.download_file(bkt_in_name, key, infile)
 
-    # Decimate the downloaded file and save as outfile.
+    if not os.path.isfile(infile):
+        raise Exception('Could not download {} from {} to {}'.format(key, bkt_in_name, infile)
+
     decimate(infile, outfile, dec_factor)
 
-    if os.path.isfile(outfile):
-        print('Uploading {} to {}'.format(outfile, bkt_out_name))
-        s3.upload_file(outfile, bkt_out_name, 'decimated/{}/{}/{}.ms'.format(year, year_day, fn))
-        #print(resp)
+    if not os.path.isfile(outfile):
+        raise Exception('Could not write output file {}'.format(outfile))
+
+    print('Uploading {} to {}'.format(outfile, bkt_out_name))
+    s3.upload_file(outfile, bkt_out_name, 'decimated/{}/{}/{}.ms'.format(year, year_day, fn))
+    os.remove(outfile)
+    os.remove(infile)
+
 
 def handler(event, context):
-    process(event)
+    """ Lambda function handler.
+    """
+    try:
+        process(event)
+    except Exception as error:
+        print(error)
+        return {'message': str(error)}
