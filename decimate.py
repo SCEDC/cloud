e6b83e7fbb7194aa4de37919676c658d1e91d641
Shang-Lin Chen <schen@gps.caltech.edu>
2019-12-20 15:50:57 -0800

    Use settings from settings.py instead of hard-coded settings.

--- decimate.py
+++ decimate.py
@@ -2,20 +2,20 @@ import boto3
 import json
 import sys
 import concurrent.futures
-
+from settings import *
 
 def call_lambda_function(client, key):
     key = key.rstrip()
     print(key)
     event = {
             's3_key': key,
-            's3_input_bucket': 'scedc-pds',
-            's3_output_bucket': 'lambda-decimated',
+            's3_input_bucket': INPUT_BUCKET,
+            's3_output_bucket': OUTPUT_BUCKET,
             'decimation_factor': 4
            }
      
     response = client.invoke(
-                               FunctionName='SCEDCFunction',
+                               FunctionName=LAMBDA_FUNCTION,
                                InvocationType='Event',
                                LogType='Tail',
                                Payload=json.dumps(event)
@@ -26,13 +26,13 @@ if len(sys.argv) < 2:
     sys.exit(2)
 keylist = sys.argv[1]
 
-session = boto3.Session(profile_name='schen-gps')
-client = session.client('lambda', region_name='us-west-2')
+session = boto3.Session(profile_name=AWS_PROFILE)
+client = session.client('lambda', region_name=AWS_REGION)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=NCORES) as executor:
     with open(keylist, 'r') as fh:
         futures = [executor.submit(call_lambda_function, client, key) for key in fh.readlines()]
         print('Executing total', len(futures), 'jobs')
-        for idx, future in enumerate(concurrent.futures.as_completed(futures, timeout=100.0)):
+        for idx, future in enumerate(concurrent.futures.as_completed(futures)):
             res = future.result()
             print('Processed job', idx, 'result', res)
