843ca50bcf9e037270d7b2cc390157481c13aadc
Aparna Bhaskaran <aparnab@copper.gps.caltech.edu>
2020-05-21 12:09:05 -0700

    feature to download using multiple processes

--- pds-fetch-continuous-data/fetch_continuous_data.py
+++ pds-fetch-continuous-data/fetch_continuous_data.py
@@ -5,10 +5,11 @@
 # 1. A file containing output of query end point of https://service.scedc.caltech.edu/fdsnws/availability/1/
 #    with format=request
 # 2. A directory to store downloaded miniseed files. Optional. If not specified, current directory is used.
+# 3. Number of processes to use for downloads. Optional. If not specified, defaults to 1.
 #
 # outputs :
 # 1. Downloaded miniseed files from s3://scedc-pds/continuous_waveforms
-# 2. A text file containing s3 locations of  downloaded files.
+# 2. A text file containing s3 locations of downloaded files.
 #
 # pre-requisites:
 # Python3, aws command line client
@@ -22,11 +23,18 @@ import os
 import shutil
 import traceback
 import subprocess
+from math import ceil
 from datetime import datetime, timedelta
+from multiprocessing import Process, cpu_count
+import time
 
+#collect all files to download in this list                                                                                                
+#pds = [[s3_file, local_file, True], [s3_file2, local_file2, True]...[s3_fileN, local_fileN, True]]                                        
+pds = []
+time2download = 0
 
 def Main():
-
+    global pds
     infile = None
     outdir = None
     pds_files = 'scedc-pds-files.txt'
@@ -46,6 +54,7 @@ def Main():
                                      '''))
     parser.add_argument("--infile", required=True, help="Input file containing requests")
     parser.add_argument("--outdir", default=os.path.dirname(os.path.abspath(__file__)), help="Location where downloaded files will be stored. It can be a local folder or s3 bucket. Default is directory where the program is located.")
+    parser.add_argument("-t", dest="processes", type=int, default=1, help="Number of processes to run the download with. For e.g. -t 2 will divide the requests between 2 processes. Default is 1.")
     args = parser.parse_args()
 
     if not os.path.exists(args.infile):
@@ -54,17 +63,19 @@ def Main():
         
     infile = args.infile
     outdir = args.outdir
+    num_processes = args.processes
 
     if not args.outdir:        
         print ("no output location provided, use current dir")
         outdir = os.path.dirname(os.path.abspath(__file__))
 
     try:
-        pds = {} #collect all files to download in this dictionary
+        #pds = {} #collect all files to download in this dictionary
         #read input file
         with open(infile, 'r') as fp:
             alllines = fp.readlines()
 
+        fp = open(pds_files, 'w')
         #process each line in the input file
         for line in alllines:
             net, sta, loc, chan, start, end = line.split(' ')
@@ -84,43 +95,71 @@ def Main():
                 ms_file = "{:s}{:_<5}{:s}{:s}_{:s}.ms".format(net, sta, chan, loc.replace('-','_'), date.strftime("%Y%j"))            
                 pds_miniseed += ms_file #download source, pds_miniseed is now s3://scedc-pds/continuous_waveforms/<year>/<year_jjj>/<net><sta><chan><loc>_YYYYJJJ.ms
                 
+                fp.write(pds_miniseed + '\n')
                 ms_file = os.path.join(outdir, ms_file) #download destination, ms_file is <net><sta><chan><loc>_YYYYJJJ.ms
-                key = date.strftime("%Y_%j")
-                if key not in pds:
-                    pds[key] = []
-                if pds_miniseed not in pds[key]:
-                    pds[key].append(pds_miniseed)
-                    pds[key].append(ms_file)
+
+                if [pds_miniseed, ms_file] not in pds:
+                    pds.append([pds_miniseed, ms_file])
                 date += timedelta(days=1)
-
-        print("Starting download from pds...")
-        start = datetime.now()
-        fp = open(pds_files,'w') #file containing list of pds files being downloaded, scedc-pds-file.txt
-        for key in pds:
-            for i in range(0, len(pds[key]), 2):
-                
-                pds_miniseed = pds[key][i]
-                ms_file = pds[key][i+1]
-
-                fp.write(pds_miniseed + '\n') #write miniseed filename to scedc-pds-files.txt
-
-                #perform the download
-                cmd = ['aws','s3','cp',pds_miniseed, ms_file]
-            
-                subprocess.call(cmd)
+        
         fp.close()
-
-        print("Download complete in ",datetime.now() -start)
-
+        
         #now copy the text file to outdir
         if outdir.find("s3://") != -1:
-            cmd = ['aws','s3','cp', pds_files, outdir]
+            cmd = ['aws','s3','cp',pds_files, outdir]
             subprocess.call(cmd)
         else:
             shutil.move(pds_files, os.path.join(outdir, pds_files))
+
+        [item.append(True) for item in pds]
+
+        print("Starting download from pds for", len(pds), "requests...")
+        step  = ceil(len(pds)/num_processes)
+        if len(pds) < num_processes:
+            print("The number of requests (", len(pds),") < number of requested processes (", num_processes,"), using 1 process instead")
+            num_processes = 1
+        
+        print ("Dividing requests between", num_processes, "process(es)")
+        
+        start_index = 0
+        end_index   = step
+        procs = []
+        
+        for i in range(0, num_processes):
+            p = Process(target=DownloadFromPDS, args=(start_index,end_index))
+            p.start()
+
+            start_index += step
+            end_index += step
+
     except:
         evalue, etype, etraceback = sys.exc_info()
         traceback.print_exception(evalue, etype, etraceback)
+
+
+def DownloadFromPDS(start, end):
+    global pds, time2download
+
+    download_start = datetime.now()
+    try:
+        for item in pds[start:end]:
+
+            pds_miniseed = item[0]                                                                                                        \
+
+            ms_file = item[1]
+            cmd = ['aws','s3','cp', pds_miniseed, ms_file]
+            #print (cmd)                                                                                                                   
+            subprocess.call(cmd)
+    except:
+        evalue, etype, etraceback = sys.exc_info()
+        traceback.print_exception(evalue, etype, etraceback)
+    
+    time2download = datetime.now() - download_start
+    if time2download.seconds < 60:
+        print ('Time to download per process is ', time2download.seconds, 'seconds')
+    else:
+        print ('Time to download per process is ', time2download.seconds/60.0, 'minutes')
+        
 
 
 if __name__ == "__main__":
