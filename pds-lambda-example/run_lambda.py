f1e0c187ba043816da89cb8f11011c5970c7abf9
Shang-Lin Chen <shanglin.chen@gmail.com>
2021-01-26 13:18:54 -0800

    Changed path of PDS waveforms. Changed SCEDC_FUNCTION to LAMBDA_FUNCTION. ADd print statement to display response.

--- pds-lambda-example/run_lambda.py
+++ pds-lambda-example/run_lambda.py
@@ -12,12 +12,14 @@ client = session.client('lambda', region_name=AWS_REGION)
 event = {
   's3_input_bucket': INPUT_BUCKET,
   's3_output_bucket': OUTPUT_BUCKET,
-  's3_key': '2016/2016_123/CIWCS2_BHE___2016123.ms',
+  's3_key': 'continuous_waveforms/2016/2016_123/CIWCS2_BHE___2016123.ms',
   'decimation_factor': 4
 }
 
+print('Invoking lambda function')
 response = client.invoke(
-        FunctionName='SCEDCFunction',
+        FunctionName=LAMBDA_FUNCTION,
         InvocationType='Event',
         LogType='Tail',
         Payload=json.dumps(event))
+print(response)
