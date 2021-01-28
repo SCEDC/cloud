c84aa3883c91dd69496a69e21483dd5f223dc38e
Shang-Lin Chen <shanglin.chen@gmail.com>
2021-01-27 19:15:53 -0800

    Replaced settings.py with settings_example.py, a sample file with placeholder default values.

--- pds-lambda-example/settings_example.py
+++ pds-lambda-example/settings_example.py
@@ -6,18 +6,18 @@ author: Shang-Lin Chen
 """
 
 AWS_PROFILE = 'default'           # Profile in .aws/credentials
-                                  # that has S3 and Lambda permissions.
+                                    # that has S3 and Lambda permissions.
 
 AWS_REGION = 'us-west-2'            # AWS region where the lambda function
                                     # is deployed. Should be the same
                                     # region as all S3 buckets.
 
 # AWS IAM role that has Lambda and S3 permissions.
-IAM_ROLE = 'iam_role'
+IAM_ROLE = 'put_iam_role_here'
 
 LAMBDA_FUNCTION = 'DecimationFunc'   # Name of your lambda function.
 
-LAMBDA_BUCKET = 'lambda_venv_bucket'  # Name of the S3 bucket that will 
+LAMBDA_BUCKET = 'lambda-venv-bucket'  # Name of the S3 bucket that will 
                                     # store a zip file containing 
                                     # the lambda function and environment.
 
@@ -29,8 +29,8 @@ INPUT_BUCKET = 'scedc-pds'          # S3 bucket used as s3_input_bucket.
                                     # This does not need to be modified
                                     # if using the SCEDC Public Data Set.
 
-OUTPUT_BUCKET = 'output_bucket'  # Name of the S3 bucket where
-                                 # output will be written.
+OUTPUT_BUCKET = 'output-bucket'  # Name of the S3 bucket where Lambda
+                                    # output will be written.
 
 NCORES = 4   # Number of cores to use for calling Lambda function.
 
