825ef142f5dfd3b9bee5bae808a19b7c1189f9a3
Shang-Lin Chen <schen@gps.caltech.edu>
2021-03-10 15:29:39 -0800

    Added support for new catalog format. Zero pad day of year when constructing prefixes for the old catalog format. Added function to get phase filename for the old catalog format. Added exception handling to continue running when some phase files are missing.

--- pds_phase_download.py
+++ pds_phase_download.py
@@ -8,33 +8,53 @@ in a particular year and month and exceed a certain magnitude.
 import pandas as pd
 import re
 import boto3
+import botocore
 
 # S3 connection
 s3cli = boto3.client('s3')
 s3res = boto3.resource('s3')
 
+BUCKET = 'scedc-pds'
+TOP_DIR = 'event_phases'
+
 # Function definitions
 
 def get_prefix(event_time):
     """ Creates the Open Data Set prefix for an event given the origin time
-    as a Pandas timestamp.
+    as a Pandas timestamp. This function is only called when the catalog file 
+    does not contain the PREFIX column.
     """
-    return 'event_phases/{}/{}_{}/'.format(event_time.year, event_time.year, event_time.dayofyear)
+
+    return '{}/{}_{:03d}/'.format(event_time.year, event_time.year, event_time.dayofyear)
+
+
+def get_phase_filename(evid):
+    """ Returns the phase filename for an event, which is evid.phase.
+    This function is only called when the catalog file does not contain the
+    PHASE_FILENAME column.
+    """
+
+    return str(evid) + '.phase'
 
 
 def get_events(year, month, mag):
     """ Get all the events in the SCEDC catalog that occurred in a given year and month and 
     exceed a given magnitude. Return the events in a dataframe.
     """
+
     # Get the catalog for the year.
-    catalog_file = '{}.catalog.csv'.format(year)
+    catalog_file = '{}_catalog_index.csv'.format(year)
     print(catalog_file)
     s3cli.download_file('scedc-pds', 'earthquake_catalogs/index/csv/year={}/{}'.format(year, catalog_file), catalog_file)
     #catalog = pd.read_csv(catalog_file, index_col=False)
     catalog = pd.read_csv(catalog_file)
 
     # Convert eventdate column from string to Timestamp.
-    catalog['eventdate'] = pd.to_datetime(catalog['YYYY/MM/DD'])
+    if 'ORIGIN_DATETIME' in catalog.columns:
+        catalog['eventdate'] = pd.to_datetime(catalog['ORIGIN_DATETIME'])
+    elif 'YYYY/MM/DD' in catalog.columns:
+        catalog['eventdate'] = pd.to_datetime(catalog['YYYY/MM/DD'])
+        
     # Get the events that meet the criteria.
     df = catalog[ (catalog['eventdate'].dt.year==year) \
                     & (catalog['eventdate'].dt.month==month) \
@@ -43,41 +63,37 @@ def get_events(year, month, mag):
     return df
 
 
-def download_objects(download_files):
+def download_object(filename):
     """ Downloads a list of files from the SCEDC Open Data Set,
     given their object keys.
     """
-    for f in download_files:
-        # The output file will just be the name of the key without the prefix.
-        output_file = f.split('/')[-1]
-        print('Downloading {} to {}.'.format(f, output_file))
-        s3cli.download_file('scedc-pds', f, output_file )
+    
+    # The output file will just be the name of the key without the prefix.
+    output_file = filename.split('/')[-1]
+    print('Downloading {} to {}.'.format(filename, output_file))
+    try:
+        s3cli.download_file('scedc-pds', filename, output_file )
+    except botocore.exceptions.ClientError as e:
+        # If an error occurred, display the messages, but continue on.
+        print('Could not download', filename)
+        print(e)
         
-        
-def list_objects(evid, prefix):
-    """ Returns a list of object names that match an event,
-    given the event ID and the Open Data Set prefix of the event.    
-    """
-    regex = re.compile('.*\/{}.*$'.format(evid))
-    obj_names = []
-    for obj in s3res.Bucket('scedc-pds').objects.filter(Prefix=prefix):
-        if regex.match(obj.key):
-            print('Match {}'.format(obj.key))
-            obj_names.append(obj.key)
-            
-    return obj_names
 
 def download_phases(year, month, mag):
     """ Download waveforms for events in the SCEDC Open Data Set that occurred in
     the given year and month and exceed the given magnitude.
     """
+
     df = get_events(year, month, mag)
-    # Determine the S3 prefix for each event.
-    df['prefix'] = df['eventdate'].apply(get_prefix)       
-    download_files = []
-    for _, row in df[['EVID', 'prefix']].iterrows():
-        download_files += list_objects(row['EVID'], row['prefix'])
-    download_objects(download_files)
+    
+    if 'PREFIX' not in df.columns:
+        df['PREFIX'] = df['eventdate'].apply(get_prefix) 
+    if 'PHASE_FILENAME' not in df.columns:
+        df['PHASE_FILENAME'] = df['EVID'].apply(get_phase_filename)
+    
+    for _, row in df.iterrows():
+        download_file = TOP_DIR + '/' + row['PREFIX'] + row['PHASE_FILENAME']
+        download_object(download_file)
 
 
 def main():
