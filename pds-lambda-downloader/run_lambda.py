775f6243f1ad1696751a70ea639030d8c57acc21
Shang-Lin Chen <schen@caltech.edu>
2021-09-10 16:33:27 -0700

    Updated

--- pds-lambda-downloader/run_lambda.py
+++ pds-lambda-downloader/run_lambda.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 run_lambda.py runs the lambda function on one seismogram.
 """
@@ -10,18 +12,19 @@ session = boto3.Session(profile_name=AWS_PROFILE)
 client = session.client('lambda', region_name=AWS_REGION)
 
 event = {
-  's3_input_bucket': INPUT_BUCKET,
-  's3_output_bucket': OUTPUT_BUCKET,
-  's3_key': 'continuous_waveforms/2016/2016_123/CIWCS2_BHE___2016123.ms',
-  'start_time': '2016/05/02T01:00:00',
-  'end_time': '2016/05/02T02:00:00',
-  'format': 'SAC'
+  "s3_input_bucket": INPUT_BUCKET,
+  "s3_output_bucket": OUTPUT_BUCKET,
+  "nscl": "CI.WCS2.BHE.  ",
+  "start_time": "2016/05/02T01:00:00",
+  "end_time": "2016/05/02T01:30:00",
+  "format": "SAC"
 }
 
 print('Invoking lambda function')
 response = client.invoke(
         FunctionName=LAMBDA_FUNCTION,
-        InvocationType='Event',
+        InvocationType='RequestResponse',
         LogType='Tail',
         Payload=json.dumps(event))
-print(response)
+#print(response)
+print(response['Payload'].read())
